d4d2aabcef7c476e8434e0fed71256dc4985dc51
SantiagoMartin2002 <150465646+SantiagoMartin2002@users.noreply.github.com>
2024-01-29 14:28:34 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{Sports_Quiz}',
+ version='1.0',
+ author='{Santiago_Martin}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
